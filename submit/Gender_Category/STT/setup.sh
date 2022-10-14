f8706aacadde4e67fcd9bbafdcc0bc1cb2cf1a06
Nattanaa <nattananfah123@gmail.com>
2022-10-14 16:30:00 +0700

    setup update

--- submit/Gender_Category/STT/setup.sh
+++ submit/Gender_Category/STT/setup.sh
@@ -1,8 +1,8 @@
 pip install gdown
 gdown --folder 1LAkmsgQ1KrxuFO54UOTnrA7NWcOGAshX
 gdown --folder 1zM_yEi0eEiAItiVSIlQeSgIGderRemHu
-gdown --folder 11bsj7DV6Y9hYf4C-Tx0P6tmvPr2hJtwsp
-gdown --folder 1TX-Fp9CWz7U2AicAjhy3gmDoM7XHqSty
+gdown --folder 1bsj7DV6Y9hYf4C-Tx0P6tmvPr2hJtwsp
+gdown 1TX-Fp9CWz7U2AicAjhy3gmDoM7XHqSty
 gdown --folder 1YPmUk3ZsfMxqq2nFwUV3fWL3uKFxz13q
 gdown --folder 19ufxw8j2jOt3t8_a3Li5tIzMI2idicVk
 gdown --folder 10DZLSO6ftUzZlvfme2FMbUIpH2ZZoYvS
