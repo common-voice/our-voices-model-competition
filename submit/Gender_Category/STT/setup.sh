5239ad123d13822a5ef0f1303dd8148d07adf626
Nattanaa <nattananfah123@gmail.com>
2022-10-14 16:16:40 +0700

    commit4

--- submit/Gender_Category/STT/setup.sh
+++ submit/Gender_Category/STT/setup.sh
@@ -2,9 +2,9 @@ pip install gdown
 gdown --folder 1LAkmsgQ1KrxuFO54UOTnrA7NWcOGAshX
 gdown --folder 1zM_yEi0eEiAItiVSIlQeSgIGderRemHu
 gdown --folder 11bsj7DV6Y9hYf4C-Tx0P6tmvPr2hJtwsp
-gdown 1TX-Fp9CWz7U2AicAjhy3gmDoM7XHqSty
-gdown 1YPmUk3ZsfMxqq2nFwUV3fWL3uKFxz13q
-gdown 19ufxw8j2jOt3t8_a3Li5tIzMI2idicVk
-gdown 10DZLSO6ftUzZlvfme2FMbUIpH2ZZoYvS
-gdown 1nsyl3VLo76DIRNg0Zrrrvy_o4QYlUtXJ
-gdown 1lBu9JD-_cQOBjsN747ElV-kAsAhR6rD6
+gdown --folder 1TX-Fp9CWz7U2AicAjhy3gmDoM7XHqSty
+gdown --folder 1YPmUk3ZsfMxqq2nFwUV3fWL3uKFxz13q
+gdown --folder 19ufxw8j2jOt3t8_a3Li5tIzMI2idicVk
+gdown --folder 10DZLSO6ftUzZlvfme2FMbUIpH2ZZoYvS
+gdown --folder 1nsyl3VLo76DIRNg0Zrrrvy_o4QYlUtXJ
+gdown --folder 1lBu9JD-_cQOBjsN747ElV-kAsAhR6rD6
