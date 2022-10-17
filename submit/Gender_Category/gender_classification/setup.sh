5b6f4f41229827c898a487db02a4ea649fcae170
nuttawatC <nuttawac@scg.com>
2022-10-17 11:13:13 +0700

    Fix setup.sh

--- submit/Gender_Category/gender_classification/setup.sh
+++ submit/Gender_Category/gender_classification/setup.sh
@@ -1,9 +1,11 @@
-# pip install git+https://github.com/speechbrain/speechbrain.git
-# pip install gdown
-# pip install ipykernel
-# pip install pandas
+pip install git+https://github.com/speechbrain/speechbrain.git
+pip install gdown
+pip install ipykernel
+pip install pandas
+
 mkdir ./models
 cd ./models
+
 gdown --folder 10wvsOq_5df1GUa8noySxVJh77DI7MV9s
 gdown --folder 1g0fiDpm_RpTIcaWHHt0qfHV2ccdi0JQf?usp=sharing
 gdown 1bdxIkmwTWlHV0n21ALJ_G9YFe6RbmkJZ
