8d61e07dc7ed6fd4d1015e65960ba6157fab7a19
nuttawatC <nuttawac@scg.com>
2022-10-19 17:48:06 +0700

    Update setup.sh & requirement

--- submit/Gender_Category/gender_classification/setup.sh
+++ submit/Gender_Category/gender_classification/setup.sh
@@ -1,13 +1,26 @@
 pip install -r ../requirements.txt
 
-mkdir ./models
+mkdir -p ./models
 cd ./models
 
-gdown --folder 10wvsOq_5df1GUa8noySxVJh77DI7MV9s
+# save
 gdown --folder 1g0fiDpm_RpTIcaWHHt0qfHV2ccdi0JQf?usp=sharing
-gdown 1bdxIkmwTWlHV0n21ALJ_G9YFe6RbmkJZ
+# pretrained_model
+gdown --folder 10wvsOq_5df1GUa8noySxVJh77DI7MV9s
+# validated.tsv
 gdown 1bVT2fSxUAXWdYHCQPWWKgjGcgHlQaSkP
-gdown 1VULKNuYZi3GQyboM52HaTA6ftTZ9CxWC
-gdown 1B7VYp_o0kz9DcY4PghHZ7sugpatiQinS
-gdown 1GjptfxivcBk92nN4TE5g28UoY4qZ9rdn
+# prediction.csv
 gdown 1FD282nxpbY3no6Jfvup3PTweQFuoUo0o
+
+# hparams_inference.yaml
+gdown 1UUUQyxUdKbTr2VA207w5SnqikDpShd5r
+
+# cv11_validated_pred.csv
+gdown 1VULKNuYZi3GQyboM52HaTA6ftTZ9CxWC
+
+# cv11+gender_prediction.csv
+gdown 1B7VYp_o0kz9DcY4PghHZ7sugpatiQinS
+
+# commonvoice11_filled_gender.csv
+gdown 1GjptfxivcBk92nN4TE5g28UoY4qZ9rdn
+# gdown 1bdxIkmwTWlHV0n21ALJ_G9YFe6RbmkJZ
